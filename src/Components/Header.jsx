18f62bcf86158d4b5df25f876a88f6d016b9fcef
WILMER <wcaceres84@unab.edu.co>
2022-12-13 03:48:35 -0500

    updateyd

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,7 +3,7 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
-import img1 from "../multimedia/HDZ_logo.PNG";
+import img1 from "../multimedia/disenio.jpg";
 
 const Header = () => {
 
